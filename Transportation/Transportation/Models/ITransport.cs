610977ce5abf3db1f75b6f893b1d5230d1f81320
OllegM <aarc@live.ru>
2019-12-28 14:15:01 +0300

    Initial commit

---
+++ Transportation/Transportation/Models/ITransport.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Transportation
+{
+  public interface ITransport<T>
+  {
+    public string getInstruction(string fromStation, string Transportation);
+  }
+}
---
+++ Transportation/Transportation/Models/Train.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Transportation {
+  class Train : ITransport
+  {
+    public string trainNumber { get; set; } //номер поезда
+    public string carNumber {get; set;} // номер вагона
+    public string sitNumber {get; set;} // место
+
+    public getInstruction (string fromStation, string toStation) {
+      return $"Take train {trainNumber} from {from} to {to}. Car {carNumber}, sit {sitNumber}.";
+    }
+  }
+}
